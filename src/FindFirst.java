5656b93c4a87f7b2fe1251287fc988e5870109c4
akhilpere <87720603+akhilpere@users.noreply.github.com>
2021-08-05 19:50:57 +0530

    Update FindFirst.java

--- src/FindFirst.java
+++ src/FindFirst.java
@@ -1,3 +1,8 @@
+// Java code for Stream findFirst()
+// which returns an Optional describing
+// the first element of this stream, or
+// an empty Optional if the stream is empty
+
 
 import java.util.*;
 
@@ -6,11 +11,17 @@ class FindFirst {
     // Driver code
     public static void main(String[] args)
     {
-
+        
+ // Creating a List of Integers
         List<Integer> list = Arrays.asList(3, 5, 7, 9, 11);
-
+        
+// Using Stream findFirst()
         Optional<Integer> answer = list.stream().findFirst();
-
+        
+        
+// if the stream is empty, an empty
+        // Optional is returned.
+        
         if (answer.isPresent()) {
             System.out.println(answer.get());
         }
