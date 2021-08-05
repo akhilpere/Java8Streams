2b1ff83207affc97e70ed6a8e065b60265140957
akhilpere <87720603+akhilpere@users.noreply.github.com>
2021-08-05 19:48:45 +0530

    Update DistinctExample.java

--- src/DistinctExample.java
+++ src/DistinctExample.java
@@ -1,3 +1,6 @@
+//   Implementation of Distinct()
+//   In Java 8
+
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
@@ -5,14 +8,17 @@ import java.util.stream.Collectors;
 
 public class DistinctExample
   {
+  //       Driver code
       public static void main(String[] args) {
+        //      Creating the list of Strings
           Collection<String> list = Arrays.asList("A", "B", "C", "D", "A", "B", "C");
-
+        
+// Get collection without duplicate i.e. distinct only
           List<String> distinctElements = list.stream()
                   .distinct()
                   .collect(Collectors.toList());
 
-
+//Let's verify distinct elements
           System.out.println(distinctElements);
       }
 }
