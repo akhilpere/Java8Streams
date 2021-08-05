6768f34c1ee371f6e5f55c258c8ad1a5ebc05af7
akhilpere <87720603+akhilpere@users.noreply.github.com>
2021-08-05 20:03:34 +0530

    Update peekExample.java

--- src/peekExample.java
+++ src/peekExample.java
@@ -1,21 +1,33 @@
+// Implementation of Stream.peek()
+// in Java 8
+
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class peekExample
  {
+ //     Driver code
      public static void main(String[] args) {
+      
+      //         creating the list of integers
          List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
 
          list.stream()
                  .peek( System.out::println );
 
          List<Integer> List = Arrays.asList(1, 2, 3, 4, 5);
-
+      
+      
+//          Using peek without any terminal
+//          operation does nothing. Hence this
+//          code will produce no output.
          List<Integer> newList = list.stream()
                  .peek(System.out::println)
                  .collect(Collectors.toList());
-
+      
+//         Printing the integers
          System.out.println(newList);
      }
 }
