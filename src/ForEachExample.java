5bd3acc0655bbec7a6eeb643d949e6e239306899
akhilpere <87720603+akhilpere@users.noreply.github.com>
2021-08-05 19:52:27 +0530

    Update ForEachExample.java

--- src/ForEachExample.java
+++ src/ForEachExample.java
@@ -1,6 +1,10 @@
+//  Java code for forEach()
+
 import java.util.ArrayList;
 import java.util.List;
 public class ForEachExample {
+    
+    //    Driver code
     public static void main(String[] args) {
         List<String> gamesList = new ArrayList<String>();
         gamesList.add("Football");
@@ -8,6 +12,11 @@ public class ForEachExample {
         gamesList.add("Chess");
         gamesList.add("Hocky");
         System.out.println("------------Iterating by passing lambda expression--------------");
+        
+        
+ //        Using  stream forEach()
+//        This method takes a single parameter which is a functional interface
+//        you can pass lambda expression as an argument.
         gamesList.forEach(games -> System.out.println(games));
 
     }
