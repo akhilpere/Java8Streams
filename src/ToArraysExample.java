e804f393550fed44ed3c5be12d81b4f45c96f071
akhilpere <87720603+akhilpere@users.noreply.github.com>
2021-08-05 19:59:13 +0530

    Update ToArraysExample.java

--- src/ToArraysExample.java
+++ src/ToArraysExample.java
@@ -1,15 +1,22 @@
-import java.util.Arrays;
+
+// Implementation of ToArrays
+// in Java 8import java.util.Arrays;
+
 import java.util.stream.Stream;
 
 public class ToArraysExample
   {
+  //      Driver code
       public static void main(String[] args)
       {
+        //           Creating a Stream of Strings
           Stream<String> stream = Stream.of("AB", "CD", "EF", "GH");
 
-
+//           Using Stream toArray()
           Object[] arr = stream.toArray();
-
+        
+        
+//        Displaying the elements in array arr
           System.out.println(Arrays.toString(arr));
       }
   }
