b58ca1c94f09a986017c0a5d35c76f7b5bc4de2a
akhilpere <87720603+akhilpere@users.noreply.github.com>
2021-08-05 19:45:36 +0530

    Update CollectExample.java

--- src/CollectExample.java
+++ src/CollectExample.java
@@ -1,10 +1,19 @@
+//  Implementation of Collect()
+//  In Java 8
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class CollectExample {
+    //    Driver code
     public static void main(String[] args) {
+        
+        //        Creating Integers
         List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
-
+        
+        
+//The collect() method of Stream class can be used to
+// Accumulate elements of any Stream into a Collection
         List<Integer> evenNumbers = numbers.stream().filter(x -> x % 2 == 0).collect(Collectors.toList());
         System.out.println(evenNumbers);
     }
