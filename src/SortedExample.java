09c88c2a9bb02b1b931a86c1db67c9b1ee3ed248
akhilpere <87720603+akhilpere@users.noreply.github.com>
2021-08-05 19:57:41 +0530

    Update SortedExample.java

--- src/SortedExample.java
+++ src/SortedExample.java
@@ -1,12 +1,24 @@
+// Implementation of Stream.sorted()
+// to get a stream of elements
+// sorted in their natural order
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class SortedExample
   {
+  // Driver code
       public static void main(String[] args) {
+        
+        // Creating a list of integers
           List<Integer> list = Arrays.asList(2, 4, 1, 3, 7, 5, 9, 6, 8);
-
+        
+        
+//          displaying the stream with elements
+//           sorted in natural order
+        
+        
           List<Integer> sortedList = list.stream()
                   .sorted()
                   .collect(Collectors.toList());
