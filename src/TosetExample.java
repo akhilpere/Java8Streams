f609b350bb6a8bfcba10e98384b370a7b2a81be8
akhilpere <87720603+akhilpere@users.noreply.github.com>
2021-08-05 20:02:00 +0530

    Update TosetExample.java

--- src/TosetExample.java
+++ src/TosetExample.java
@@ -1,18 +1,28 @@
+
+// Java code to show the implementation of
+// Collectors toSet() function
+
+
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TosetExample
    {
+   // Driver code
        public static void main(String[] args) {
+          
+          // creating a Stream of strings
            Stream<String> s = Stream.of("Babu",
                    "Siva",
                    "BabuSiva",
                    "Babu Siva");
 
-
+// using Collectors toSet() function
            Set<String> mySet = s.collect(Collectors.toSet());
-
+          
+          
+// printing the elements
            System.out.println(mySet);
        }
 }
